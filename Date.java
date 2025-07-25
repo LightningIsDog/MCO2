1f6c16af9639d47057c5f09ce54e8862bef5539c
LightningIsDog <codexliliumconvallium@gmail.com>
2025-07-25 17:54:22 +0800

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,33 @@
+public class Date {
+    private int month;
+    private int day;
+    private int year;
+
+    public Date(int month, int day, int year) {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+    public void display() {
+        String m = String.valueOf(month);
+        String d = String.valueOf(day);
+        String y = String.valueOf(year);
+
+        System.out.println(m + "/" + d + "/" + y);
+    }
+}
